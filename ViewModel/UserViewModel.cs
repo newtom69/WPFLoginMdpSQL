3442bb34fa5715da4a30eed3775711f527594d14
Thomas Vuille <tvuille@yahoo.fr>
2018-10-15 10:53:20 +0200

    ajout UserViewModel

---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+using Ressources.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ressources.ViewModel
+{
+    class UserViewModel : BindableBase
+    {
+        private UserModel _UserModelData;
+
+        public UserModel UserModelData
+        {
+            get => _UserModelData;
+            set => SetProperty(ref _UserModelData, value);
+        }
+    }
+}
