77c1cb4b01f115efe4817e0205c6db93f38a21ec
Thomas Vuille <tvuille@yahoo.fr>
2018-10-15 10:26:24 +0200

    ajout Classe UserModel

---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace Ressources.Models
+{
+    public class UserModel : BindableBase
+    {
+        private string _Name;
+
+        public string Name { get => _Name; set => _Name = value; }
+    }
+}
